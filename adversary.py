0ed68289eee89be4d9295a6e408b0200392c3d16
Aman Deep Singh <aman150084@gmail.com>
2019-12-12 04:43:01 +0530

    changed the return value

---
+++ adversary.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.nn import Parameter
+import math
+
+class MaskMan(nn.Module):
+    def __init__(self,  in_features):
+        super(MaskMan, self).__init__()
+        self.in_features = in_features
+        self.conv1_1 = nn.Conv2d(in_features,in_features,3,1,1) #=>B*64*56*48
+        self.relu1_1 = nn.PReLU(in_features)
+        self.conv1_2 = nn.Conv2d(in_features,in_features,3,1,1)
+        self.relu1_2 = nn.PReLU(in_features)
+        self.conv1_3 = nn.Conv2d(in_features,1,3,1,1)
+        self.sigmoid = nn.Sigmoid()
+        
+
+# import torch
+# import torch.nn as nn
+# from torch.autograd import Variable
+# import torch.nn.functional as F
+# from torch.nn import Parameter
+# import math
+# a = torch.rand((3,2)).type(torch.DoubleTensor)
+# print(a)
+# b = torch.randint(2, (3, 2)).type(torch.DoubleTensor)
+# print(b)
+# print(torch.mul(a, b))
+# TODO: Check thether this is in_features or in_channels
+    # Returns the mask. Use torch.mul(a,b) to get the resultant image. a and b are of same dimension
+    # change: Return the masked image(not the mask)
+    def forward(self, x):
+        x_ = x
+        x = self.relu1_1(self.conv1_1(x))
+        x = self.relu1_2(self.conv1_2(x))
+        x = self.sigmoid(self.conv1_3(x))
+        x = torch.mul(x, x_) #this step multiplies the mask with image
+        
+        return x
+
