aa02058adeabb0dff2583bd50d7feee2267ea2ae
Aman Deep Singh <aman150084@gmail.com>
2020-02-10 16:27:08 +0530

    changes ratio of loss

--- train.py
+++ train.py
@@ -82,8 +82,8 @@ def train(epoch,args):
             lossSize = F.l1_loss(mask, target=torch.ones(mask.size()), size_average = False)
         writer.add_scalar('Loss/adv-classification', -lossAdv/10, n_iter)
         writer.add_scalar('Loss/adv-compactness', lossCompact/1000000, n_iter)
-        writer.add_scalar('Loss/adv-size', lossSize/10000, n_iter)
-        loss = -lossAdv/10 + lossCompact/1000000 + lossSize/10000
+        writer.add_scalar('Loss/adv-size', lossSize/1000, n_iter)
+        loss = -lossAdv/10 + lossCompact/1000000 + lossSize/1000
         # loss = - criterion2(outputs1, targets)/100 + lossCompact/1000000 + lossSize/10000
         writer.add_scalar('Accuracy/adv-totalLoss', loss, n_iter)
         lossd = loss.data
