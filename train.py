36b4f831c4fce2e4430307796e0297913aeefd1c
Aman Deep Singh <aman150084@gmail.com>
2020-02-11 19:36:28 +0530

    changes

--- train.py
+++ train.py
@@ -85,8 +85,8 @@ def train(epoch,args):
             lossSize = F.l1_loss(mask, target=torch.ones(mask.size()), size_average = False)
         writer.add_scalar('Loss/adv-classification', -lossAdv/10, n_iter)
         writer.add_scalar('Loss/adv-compactness', lossCompact/1000000, n_iter)
-        writer.add_scalar('Loss/adv-size', lossSize/10000, n_iter)
-        loss = -lossAdv/10  + lossSize/10000
+        writer.add_scalar('Loss/adv-size', lossSize/1000000, n_iter)
+        loss = -lossAdv/10  + lossSize/1000000
         
         # loss = -lossAdv/10 + lossCompact/1000000 + lossSize/10
         # loss = - criterion2(outputs1, targets)/100 + lossCompact/1000000 + lossSize/10000
