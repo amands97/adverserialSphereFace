2fbe74bece84139901a3bd35418610fa6999ffd3
Aman Deep Singh <aman150084@gmail.com>
2020-02-11 21:08:37 +0530

    changes

--- train.py
+++ train.py
@@ -173,7 +173,7 @@ print('start: time={}'.format(dt()))
 for epoch in range(0, 50):
     if epoch in [0,10,15]:
         if epoch!=0: args.lr *= 0.1
-        optimizerFC = optim.SGD(list(featureNet.parameters()) + list(fcNet.parameters()), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+        optimizerFC = optim.SGD(list(featureNet.parameters()) + list(fcNet.parameters()), lr=args.lr/100, momentum=0.9, weight_decay=5e-4)
         # optimizerFeature = optim.SGD(featureNet.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
         optimizerMask = optim.SGD(maskNet.parameters(), lr = args.lr, momentum=0.9, weight_decay=5e-4)
         # slowed the lr even more
