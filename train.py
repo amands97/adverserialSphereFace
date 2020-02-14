b572bd9c92c449b4a74152ed559c0c9f5ab2e27c
Aman Deep Singh <aman150084@gmail.com>
2020-02-14 11:45:55 +0530

    changes

--- train.py
+++ train.py
@@ -103,7 +103,7 @@ def train(epoch,args):
         writer.add_scalar('Loss/adv-classification', -lossAdv/10, n_iter)
         writer.add_scalar('Loss/adv-compactness', lossCompact/1000000, n_iter)
         writer.add_scalar('Loss/adv-size', lossSize/1000000, n_iter)
-        if lossSize/10000 < 0.1:
+        if lossSize/10000 < 0.5:
             loss = -lossAdv/10
         else:
             loss = -lossAdv/10  + lossSize/10000
