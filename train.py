576482064e08564ce3e2662fe7ad2a12339d1a8a
Aman Deep Singh <aman150084@gmail.com>
2020-02-15 16:48:56 +0530

    changes

--- train.py
+++ train.py
@@ -106,7 +106,7 @@ def train(epoch,args):
         if lossSize/10000 < 0.5:
             loss = -lossAdv/10
         else:
-            loss = -lossAdv/10  + lossSize/100000
+            loss = -lossAdv/10  + lossSize/10000
         
         # loss = -lossAdv/10 + lossCompact/1000000 + lossSize/10
         # loss = - criterion2(outputs1, targets)/100 + lossCompact/1000000 + lossSize/10000
