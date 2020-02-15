42fb7a1ab7591ba2dfe3cb6a4b91d11e43ee6453
Aman Deep Singh <aman150084@gmail.com>
2020-02-15 19:56:25 +0530

    changed l1 loss

--- train.py
+++ train.py
@@ -192,7 +192,7 @@ if use_cuda:
     laplacianKernel =  laplacianKernel.cuda()
 
 criterion = net_sphere.AngleLoss()
-optimizerFC = optim.SGD(list(featureNet.parameters()) + list(fcNet.parameters()), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+optimizerFC = optim.SGD(list(featureNet.parameters()) + list(fcNet.parameters()), lr=args.lr, momentum=0.09, weight_decay=5e-4)
         # optimizerFeature = optim.SGD(featureNet.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 optimizerMask = optim.SGD(maskNet.parameters(), lr = args.lr/1000, momentum=0.9, weight_decay=5e-4)
 criterion2 = torch.nn.CrossEntropyLoss()
