ab8e7d17fbd90176c8cf1e92c078880712382c5f
Aman Deep Singh <aman150084@gmail.com>
2020-05-01 07:05:10 +0530

    Added the compactness loss

--- try.py
+++ try.py
@@ -1,8 +1,8 @@
 import torch
 from torch.nn.functional import conv2d
-x = torch.ones((1,512, 7, 6))
-# x[0, 0, 0, 0] = 0
-# x[0, 0, 0, 1] = 0
+x = torch.ones((1,1, 112, 96))
+x[0, 0, 1, 0] = 0
+x[0, 0, 1, 2] = 0
 
 print(x)
 kernel = torch.ones((3,3))
@@ -10,8 +10,9 @@ mid = 1
 kernel[mid, mid] = -8
 kernel = kernel/-8
 print(kernel)
-
 b, c, h, w = x.shape
+print(c)
+# c = 1
 kernel = kernel.type(torch.FloatTensor)
 kernel = kernel.repeat(c, 1, 1, 1)
 ans = conv2d(x, kernel, stride=1, groups=c)
