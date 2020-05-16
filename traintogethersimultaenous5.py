724a91c90937a57fde0224ac0b10ba4333aa6197
amands <aman150084@gmail.com>
2020-05-16 14:59:36 +0530

    removed the if condition

--- traintogethersimultaenous5.py
+++ traintogethersimultaenous5.py
@@ -22,8 +22,8 @@ import net_sphere
 import adversary
 from gumbel import gumbel_softmax
 from torch.nn.functional import conv2d # for the kernel
-# from torch.utils.tensorboard import SummaryWriter
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
+# from tensorboardX import SummaryWriter
 from aux import *
 # to import all the necessary changes
 parser = argparse.ArgumentParser(description='PyTorch sphereface')
