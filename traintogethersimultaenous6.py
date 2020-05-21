810e0a902a6df7c0e95396f1df8786e57bb3b5aa
amands <aman150084@gmail.com>
2020-05-21 22:59:08 +0530

    push

--- traintogethersimultaenous6.py
+++ traintogethersimultaenous6.py
@@ -162,7 +162,7 @@ def train(epoch,args):
 
         # writer.add_scalar('Loss/adv-compactness', lossCompact/10, n_iter)
         writer.add_scalar('mask/adv-size', lossSize, n_iter)
-        loss = (-lossAdv)  + lossSize/1000
+        loss = (-lossAdv)  + lossSize/100
         writer.add_scalar('mask/adv-totalLoss', loss, n_iter)
         # lossd = loss.data
         loss.backward()
