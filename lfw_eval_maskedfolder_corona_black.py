f4f90bb59db7cdaacda02cd9a2ecf2d921f67222
amands <aman150084@gmail.com>
2020-05-29 04:46:31 +0530

    corona black

--- lfw_eval_maskedfolder_corona_black.py
+++ lfw_eval_maskedfolder_corona_black.py
@@ -207,10 +207,16 @@ for i in range(len(pairs_lines)):
     # mask2 = cv2.fromarray(mask2)
     mask2 = Image.fromarray(mask2.astype(np.uint8))
 
+    # print(mask1.shape)
+    # mask1 = cv2.resize(mask1, (96, 112))
+    # print(mask1)
+    # mask2 = cv2.resize(mask2, (96, 112))
+    mask1 = mask1.resize((96, 112))
+    mask2 = mask2.resize((96, 112))
+    
+    mask1 = np.array(mask1)
+    mask2 = np.array(mask2)
     print(mask1.shape)
-    mask1 = cv2.resize(mask1, (96, 112))
-    print(mask1)
-    mask2 = cv2.resize(mask2, (96, 112))
     img1 = cv2.resize(img1, (96, 112))
     img2 = cv2.resize(img2, (96, 112))
     mask1 = mask1[..., np.newaxis]
