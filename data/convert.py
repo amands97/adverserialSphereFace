159f5e363524ded5822da246476f49a5f97315dc
amands <aman150084@gmail.com>
2020-05-30 01:52:48 +0530

    convert script

--- data/convert.py
+++ data/convert.py
@@ -3,13 +3,21 @@
 import zipfile
 import csv
 zfile = zipfile.ZipFile("../masked_casia.zip")
-
+writeFile = open("masked_casia_landmark.txt", "w")
+idx = 0
 with open("casia_landmark.txt") as f:
     lines = f.readlines()
     # print(lines[0].split("\t"))
     for line in lines:
+        idx += 1
+        if idx % 5000 == 0:
+            print(idx)
         filename = line.split("\t")[0]
         try:
             zfile.open(filename)
+            # writeFile.write("")
+            print >>writeFile, line
         except:
             print(filename)
+            # writeFile.write(filename + "")
+writeFile.close()
